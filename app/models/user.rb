e1c370793a8efba3d609bccf7428b1270a19fe0e
ashernor <ashernor@gmail.com>
2013-08-25 22:59:03 +0200

    registration is limited to fidzup domain email and internalization

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ActiveRecord::Base
   validates_confirmation_of :password
   validates_presence_of :email, :first_name, :last_name, :first_name, :last_name
   validates_uniqueness_of :email
+  validates_format_of :email, :with => /fidzup.com$/
 
   has_many :debts, :class_name => "Debt", :foreign_key => :debtor_id
   has_many :credits, :class_name => "Debt", :foreign_key => :creditor_id
